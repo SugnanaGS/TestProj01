a964f283c767e821194b63b7b5ab39a90ac85944
sugnanags <sugnanags@gmail.com>
2026-09-25 11:35:35 +0530

    1st Commit 25Sep2026

--- TestMain01.java
+++ TestMain01.java
@@ -2,5 +2,6 @@ public class TestMain01{
 	public static void main(String args[]){
 		System.out.println("Hello from TestMain01 Java Application");
 		System.out.println("Updating new Business Logic for TestMain01 Application");
+		System.out.println("Demonstrating Git Web Hooks Trigger for Jenkins");
 	}
 }
